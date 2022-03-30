b327eb53e9684c860d2039941164b992f71af4a1
Akhilesh Muthusamy <AkhileshM@outlook.in>
2022-03-29 23:11:45 -0400

    Filter students endpoint

--- routes/placement/skill.js
+++ routes/placement/skill.js
@@ -15,4 +15,32 @@ router.get('/', verifyToken, (req, res) => {
     }).catch(err => internalError(res, err));
 });
 
+router.post('/student', verifyToken, (req, res) => {
+
+    if (!req.body.skills || !req.body.match) {
+        return res.status(400).json({error: true, message: 'One or more required field missing' });
+    }
+
+    const {skills, match, batch, department, cgpa} = req.body;
+    let query = {};
+
+    if (match === 'ANY') {
+        query = {'skills': {$in: skills}};
+    } else if (match === 'ALL') {
+        query = {$and: skills.map(skill => { return {'skills': skill}})};
+    }
+    if (cgpa && cgpa > 0) query['cgpa'] = {$gte: cgpa};
+    if (batch && batch.length > 0) query['batch'] = {$in: batch};
+    if (department && department.length > 0) query['department'] = {$in: department};
+
+    User.find(query).then(users => {
+
+        res.status(200).json({
+            data: users,
+            error: false,
+        });
+
+    }).catch(err => internalError(res, err));
+});
+
 module.exports = router;
