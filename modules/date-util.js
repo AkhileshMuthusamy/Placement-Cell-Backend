04c36aa5f179947d0e96c76aad936bf310cb91d0
Akhilesh Muthusamy <AkhileshM@outlook.in>
2022-04-24 18:37:15 -0400

    Fixed bug

--- modules/date-util.js
+++ modules/date-util.js
@@ -2,17 +2,16 @@
 
 function getDateTimeString() {
     let current = new Date();
-    let cDate = current.getFullYear() + (current.getMonth() + 1) + current.getDate();
-    let cTime = current.getHours() + current.getMinutes() + current.getSeconds();
+    let cDate = `${current.getFullYear()}` + `${(current.getMonth() + 1)}` + `${current.getDate()}`;
+    let cTime = `${current.getHours()}` + `${current.getMinutes()}` + `${current.getSeconds()}`;
     let dateTime = cDate + cTime;
     return dateTime;
 }
 
 function getDateString() {
     let current = new Date();
-    let cDate = current.getFullYear() + (current.getMonth() + 1) + current.getDate();
-    let dateTime = cDate;
-    return dateTime;
+    let cDate = `${current.getFullYear()}` + `${(current.getMonth() + 1)}` + `${current.getDate()}`;
+    return cDate;
 }
 
 module.exports = {
